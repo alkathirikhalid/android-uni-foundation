6f9f3dae9124ef3bcca7b6ec6cbd0e14f90760e6
alkathirikhalid <khalid@alkathirikhalid.com>
2015-11-25 12:26:54 +0800

    Modified Activity.java to include the View Objects from our XML file

--- app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
+++ app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
@@ -2,12 +2,19 @@ package com.alkathirikhalid.audiorecorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonRecord, buttonPlay;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        buttonRecord = (Button) findViewById(R.id.buttonRecord);
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
     }
 }
