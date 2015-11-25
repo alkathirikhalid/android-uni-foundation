1a7722d82745f93ae53e0b18b651856c7baa81f0
alkathirikhalid <khalid@alkathirikhalid.com>
2015-11-25 13:45:29 +0800

    Modified MainActivity.java to include FileName, Enviroment Path, MediaRecorder and MediaPlayer

--- app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
+++ app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
@@ -1,14 +1,20 @@
 package com.alkathirikhalid.audiorecorder;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonRecord, buttonPlay;
     private ImageView imageView;
+    private static String fileName;
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonRecord.setOnClickListener(this);
         buttonPlay = (Button) findViewById(R.id.buttonPlay);
         buttonPlay.setOnClickListener(this);
+        fileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "audiorecorder.3gp";
     }
 
     @Override
