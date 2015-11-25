0f1cafeb2e75bd38a96552cc54040f266862980a
alkathirikhalid <khalid@alkathirikhalid.com>
2015-11-25 12:45:37 +0800

    Modified MainActivity by Adding OnClickListener and Binding it to the View Buttons.

--- app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
+++ app/src/main/java/com/alkathirikhalid/audiorecorder/MainActivity.java
@@ -2,10 +2,11 @@ package com.alkathirikhalid.audiorecorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button buttonRecord, buttonPlay;
     private ImageView imageView;
 
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.imageView);
         buttonRecord = (Button) findViewById(R.id.buttonRecord);
+        buttonRecord.setOnClickListener(this);
         buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
